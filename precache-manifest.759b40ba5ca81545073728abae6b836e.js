92eb4728f4e589097c8a1a44d69da10dc2bcb382
onerri <77608136+onerri@users.noreply.github.com>
2021-07-27 06:32:38 +0100

    Add files via upload

---
+++ precache-manifest.759b40ba5ca81545073728abae6b836e.js
@@ -0,0 +1,61 @@
+self.__precacheManifest = [
+  {
+    "revision": "6cc023608295115714b1",
+    "url": "/assets/css/chunk-vendors.be8ee406.css"
+  },
+  {
+    "revision": "6cc023608295115714b1",
+    "url": "/assets/js/chunk-vendors.8b59faa9.js"
+  },
+  {
+    "revision": "bc43022f48eb068ef374",
+    "url": "/assets/css/index.65ee3396.css"
+  },
+  {
+    "url": "/index-a661ef1fbfe6005b02e5.js"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/assets/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/assets/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "d0066537ab6a4c6f8285a5aeb3ba5f09",
+    "url": "/assets/fonts/materialdesignicons-webfont.d0066537.woff2"
+  },
+  {
+    "revision": "b4917be25082eb793b5363f2fdb5f282",
+    "url": "/assets/fonts/materialdesignicons-webfont.b4917be2.woff"
+  },
+  {
+    "revision": "2d0a0d8f5f173be15a67aa084db94fe6",
+    "url": "/assets/fonts/materialdesignicons-webfont.2d0a0d8f.eot"
+  },
+  {
+    "revision": "f51112347be6b44f9ef46151a971430d",
+    "url": "/assets/fonts/materialdesignicons-webfont.f5111234.ttf"
+  },
+  {
+    "revision": "96c476804d7a788cc1c05351b287ee41",
+    "url": "/assets/fonts/MaterialIcons-Regular.96c47680.eot"
+  },
+  {
+    "revision": "0509ab09c1b0d2200a4135803c91d6ce",
+    "url": "/assets/fonts/MaterialIcons-Regular.0509ab09.woff2"
+  },
+  {
+    "revision": "29b882f018fa6fe75fd338aaae6235b8",
+    "url": "/assets/fonts/MaterialIcons-Regular.29b882f0.woff"
+  },
+  {
+    "revision": "da4ea5cdfca6b3baab285741f5ccb59f",
+    "url": "/assets/fonts/MaterialIcons-Regular.da4ea5cd.ttf"
+  },
+  {
+    "revision": "d8de5694069578124e27a86ff4f4366c",
+    "url": "/index.html"
+  }
+];
